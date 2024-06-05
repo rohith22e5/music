479b25de5e809d2ff0c9d5100d32306dc670a243
rohith <rohithreddy1gaddam@gmail.com>
2024-06-05 16:38:27 +0530

    logout done prolly 95

--- spotify/static/spotify/js/index.js
+++ spotify/static/spotify/js/index.js
@@ -1,6 +1,13 @@
 // cardClicks.js
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            link.parentElement.classList.add('active');
+        }
+    });
         document.getElementById('prevPage').addEventListener('click', function(e) {
             e.preventDefault();
             window.history.back();
